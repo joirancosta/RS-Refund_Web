b342f876fa7499dfafd783819c568fcf9ea6a3da
“joirancosta” <joiran.costa.jc@gmail.com>
2025-07-31 23:02:22 -0300

    add confirm

--- web/src/components/AppLayout.tsx
+++ web/src/components/AppLayout.tsx
@@ -1,6 +1,6 @@
 
-import { Outlet } from "react-router";
 import { Header } from "./Header";
+import { Outlet } from "react-router";
 
 export function AppLayout() {
   return (
--- web/src/routes/EmployeeRoutes.tsx
+++ web/src/routes/EmployeeRoutes.tsx
@@ -1,15 +1,17 @@
 
 import { Routes, Route } from "react-router";
 
-import { AppLayout } from "../components/AppLayout";
 import { Refund } from "../pages/Refund";
 import { NotFound } from "../pages/NotFound";
+import { AppLayout } from "../components/AppLayout";
+import { Confirm } from "../pages/Confirm";
 
 export function EmployeeRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route path="/" element={<Refund />} />
+        <Route path="/confirm" element={<Confirm />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
