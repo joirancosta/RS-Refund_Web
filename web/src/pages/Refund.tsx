c4b0a862a62c534c911c435e9c34998bc237f271
“joirancosta” <joiran.costa.jc@gmail.com>
2025-07-31 23:05:30 -0300

    corrigindo refund

--- web/src/pages/Refund.tsx
+++ web/src/pages/Refund.tsx
@@ -25,27 +25,27 @@ export function Refund() {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <header className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+    <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+      <header>
         <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso</p>
-
-        <Input required legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)} />
-        <div className="flex gap-4">
-          <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
-            {
-              CATEGORIES_KEYS.map((category) => (
-                <option key={category} value={category}>
-                  { CATEGORIES[category].name }
-                </option>
-              ))
-            }
-          </Select>
-          <Input required legend="Valor" value={amount} onChange={(e) => setAmount(e.target.value)}/>
-        </div>
-        <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])} />
-        <Button type="submit" isLoading={isLoading}>Enviar</Button>
       </header>
+
+      <Input required legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)} />
+      <div className="flex gap-4">
+        <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
+          {
+            CATEGORIES_KEYS.map((category) => (
+              <option key={category} value={category}>
+                { CATEGORIES[category].name }
+              </option>
+            ))
+          }
+        </Select>
+        <Input required legend="Valor" value={amount} onChange={(e) => setAmount(e.target.value)}/>
+      </div>
+      <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])} />
+      <Button type="submit" isLoading={isLoading}>Enviar</Button>
     </form>
   )
 }
