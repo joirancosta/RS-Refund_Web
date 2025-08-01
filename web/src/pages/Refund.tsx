3e9572f53767f0db31fa423d5abe4ba14341b0bf
“joirancosta” <joiran.costa.jc@gmail.com>
2025-07-31 21:10:55 -0300

    add solicitação de reembolso

---
+++ web/src/pages/Refund.tsx
@@ -0,0 +1,35 @@
+
+import { useState } from "react"
+
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
+
+import { Input } from "../components/Input"
+import { Select } from "../components/Select"
+import { Upload } from "../components/Upload"
+
+export function Refund() {
+  const [category, setCategory] = useState("")
+  return (
+    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+      <header>
+        <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
+        <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso</p>
+
+        <Input required legend="Nome da solicitação"/>
+        <div className="flex gap-4">
+          <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
+            {
+              CATEGORIES_KEYS.map((category) => (
+                <option key={category} value={category}>
+                  { CATEGORIES[category].name }
+                </option>
+              ))
+            }
+          </Select>
+          <Input required legend="Valor"/>
+        </div>
+        <Upload />
+      </header>
+    </form>
+  )
+}
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,11 +2,12 @@
 import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./auth-routes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <EmployeeRoutes />
     </BrowserRouter>
   )
 }
